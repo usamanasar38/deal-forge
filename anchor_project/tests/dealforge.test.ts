e3c9a4c2b8044b1685e58fc03c4b4b29f543c331
Usama Nasar <usamanasar38@gmail.com>
2025-09-05 22:57:05 +0500

    test: make offer unit test

--- anchor_project/tests/dealforge.test.ts
+++ anchor_project/tests/dealforge.test.ts
@@ -1,12 +1,16 @@
-import {
-  type Blockhash,
-  createSolanaClient,
-  createTransaction,
-  type Instruction,
-  type KeyPairSigner,
-  signTransactionMessageWithSigners,
-} from "gill";
+import type { Address, KeyPairSigner } from "gill";
 import { loadKeypairSignerFromFile } from "gill/node";
+import { getAssociatedTokenAccountAddress } from "gill/programs";
+import { beforeAll, describe, expect, it } from "vitest";
+import {
+  createTestOffer,
+  createToekn,
+  createWalletWithSol,
+  getTokenAccountBalance,
+  mintTokens,
+  ONE_MINT_TOKEN,
+  tokenProgram,
+} from "./utils";
 
 /** Turn on debug mode */
 global.__GILL_DEBUG__ = true;
@@ -14,23 +18,76 @@ global.__GILL_DEBUG__ = true;
 /** Set the debug mode log level (default: `info`) */
 global.__GILL_DEBUG_LEVEL__ = "debug";
 
+const aliceInitialTokenAAmount = 100n;
+const bobInitialTokenAAmount = 10n;
+const bobInitialTokenBAmount = 10n;
+const tokenAOfferedAmount = 1n * ONE_MINT_TOKEN;
+const tokenBWantedAmount = 1n * ONE_MINT_TOKEN;
+
 describe("dealforge", () => {
-  let payer: KeyPairSigner;
+  let signer: KeyPairSigner;
+  let alice: KeyPairSigner;
+  let bob: KeyPairSigner;
+  let tokenMintA: KeyPairSigner;
+  let tokenMintB: KeyPairSigner;
+  let aliceTokenAccountA: Address;
+  let aliceTokenAccountB: Address;
+  let bobTokenAccountA: Address;
+  let bobTokenAccountB: Address;
 
   beforeAll(async () => {
-    payer = await loadKeypairSignerFromFile(process.env.ANCHOR_WALLET!);
+    signer = await loadKeypairSignerFromFile(process.env.ANCHOR_WALLET);
+    alice = await createWalletWithSol();
+    bob = await createWalletWithSol();
+    // Create Tokens
+    tokenMintA = await createToekn(signer);
+    tokenMintB = await createToekn(signer);
+
+    // Mint tokens A and save their address
+    aliceTokenAccountA = await mintTokens({
+      signer,
+      destinationWallet: alice,
+      mint: tokenMintA,
+      amount: aliceInitialTokenAAmount,
+    });
+
+    bobTokenAccountA = await mintTokens({
+      signer,
+      destinationWallet: bob,
+      mint: tokenMintA,
+      amount: bobInitialTokenAAmount,
+    });
+
+    // Mint token B and save their addresses
+    bobTokenAccountB = await mintTokens({
+      signer,
+      destinationWallet: bob,
+      mint: tokenMintB,
+      amount: bobInitialTokenBAmount,
+    });
+
+    // get alice token B address
+    aliceTokenAccountB = await getAssociatedTokenAccountAddress(
+      tokenMintB,
+      alice,
+      tokenProgram
+    );
   });
 
-  it('should run the program and print "GM!" to the transaction log', async () => {
-    // ARRANGE
-    expect.assertions(1);
-    const ix = getGreetInstruction();
+  describe("makeOffer", () => {
+    it("successfully creates an offer with valid inputs", async () => {
+      const { vault } = await createTestOffer({
+        maker: alice,
+        offeredMint: tokenMintA,
+        requestedMint: tokenMintB,
+        makerTokenAccount: aliceTokenAccountA,
+        tokenOfferedAmount: tokenAOfferedAmount,
+        tokenRequestedAmount: tokenBWantedAmount,
+      });
 
-    // ACT
-    const sx = await sendAndConfirm({ ix, payer });
-
-    // ASSERT
-    expect(sx).toBeDefined();
-    console.log("Transaction signature:", sx);
+      // Verify the offer was created successfully by checking the vault balance
+      const vaultBalanceResponse = await getTokenAccountBalance(vault);
+      expect(BigInt(vaultBalanceResponse.amount)).toEqual(tokenAOfferedAmount);
+    });
   });
 });
